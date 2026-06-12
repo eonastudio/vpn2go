d7a4dc4dab264627618e26e3ddb8e9cdaf6983e8
VPN2GO <vpn2go@build.local>
2026-06-13 00:15:11 +0300

    Simplify MainActivity - remove singbox refs for now

--- flutter_app/android/app/src/main/kotlin/com/vpn2go/app/MainActivity.kt
+++ flutter_app/android/app/src/main/kotlin/com/vpn2go/app/MainActivity.kt
@@ -1,26 +1,13 @@
 package com.vpn2go.app
 
-import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
-import com.vpn2go.singbox.SingBoxBridge
 
 class MainActivity : FlutterActivity() {
-    private lateinit var singBoxBridge: SingBoxBridge
-
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        singBoxBridge = SingBoxBridge(this)
-        singBoxBridge.configure(flutterEngine)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        singBoxBridge.onActivityResult(requestCode, resultCode)
-    }
-
-    override fun onDestroy() {
-        singBoxBridge.dispose()
-        super.onDestroy()
+        // TODO: Подключить sing-box bridge когда libbox.aar будет добавлен
+        // singBoxBridge = SingBoxBridge(this)
+        // singBoxBridge.configure(flutterEngine)
     }
 }
